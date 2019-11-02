3a62a034a2a7cf7dc421e1468b06641bee742e65
nilsjha <nils@gaupne.net>
2019-11-03 00:56:19 +0100

    Implemented GET-request method

--- src/main/java/no/ntnu/nilsjarh/rest/App.java
+++ src/main/java/no/ntnu/nilsjarh/rest/App.java
@@ -3,7 +3,11 @@ package no.ntnu.nilsjarh.rest;
 public class App {
     
     public static void main(String[] args) {
-    
+  
+        // demo to datakomm.work
+        RestClient demo = new RestClient();
+        demo.setEndpointUrl("datakomm.work",80,false);
+        System.out.println(demo.send("dkrest/test/get"));
     }
     
 }
