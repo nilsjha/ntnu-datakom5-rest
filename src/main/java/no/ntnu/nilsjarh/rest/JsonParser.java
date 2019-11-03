d6a1871b69937695e337b0298a27c6076122bcec
nilsjha <nils@gaupne.net>
2019-11-03 14:48:54 +0100

    added methods to extract int and String from JSON

--- src/main/java/no/ntnu/nilsjarh/rest/JsonParser.java
+++ src/main/java/no/ntnu/nilsjarh/rest/JsonParser.java
@@ -1,4 +1,42 @@
 package no.ntnu.nilsjarh.rest;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class jsonParser {
+public class JsonParser {
+    public int extractInt(String jsonInput, String key) {
+        int result = 0;
+        try {
+            JSONObject jsonObj = new JSONObject(jsonInput);
+            result = jsonObj.getInt(key);
+        } catch (JSONException e) {
+            System.err.println("Unable to parse JSON: " + e.getMessage());
+        }
+        return result;
+    }
+    public String extractString(String jsonInput, String key) {
+        String result = "";
+        try {
+            JSONObject jsonObj = new JSONObject(jsonInput);
+            result = jsonObj.getString(key);
+        } catch (JSONException e) {
+            System.err.println("Unable to parse JSON: " + e.getMessage());
+        }
+        return result;
+    }
+    
+    /**
+     *  Sorts the JSON elements in an encoded string
+     * @param jsonInput String with JSON-encoded elements
+     * @return String with the JSON elements sorted alphabetically
+     */
+    public String sortJsonString(String jsonInput) {
+       try {
+           JSONObject jsonObj = new JSONObject(jsonInput);
+           return jsonObj.toString();
+       } catch (JSONException e) {
+           System.err.println("Unable to parse JSON: " + e.getMessage());
+           return "";
+       }
+    }
+    
 }
