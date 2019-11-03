5ab09e3a44b38aba2c881a464645971ff6cc4e6a
nilsjha <nils@gaupne.net>
2019-11-03 14:56:43 +0100

    added test methods to main app method

--- src/main/java/no/ntnu/nilsjarh/rest/App.java
+++ src/main/java/no/ntnu/nilsjarh/rest/App.java
@@ -7,7 +7,15 @@ public class App {
         // demo to datakomm.work
         RestClient demo = new RestClient();
         demo.setEndpointUrl("datakomm.work",80,false);
-        System.out.println(demo.send("dkrest/test/get"));
+        String getDemo1 = demo.send("dkrest/test/get");
+        String getDemo2 = demo.send("dkrest/test/get2");
+        System.out.println(getDemo1);
+        System.out.println(getDemo2);
+        JsonParser parser = new JsonParser();
+        int parsed = parser.extractInt(getDemo2,"a");
+        String sortedDemo2 = parser.sortJsonString(getDemo2);
+        System.out.println(sortedDemo2);
+        System.out.println(parsed);
     }
     
 }
