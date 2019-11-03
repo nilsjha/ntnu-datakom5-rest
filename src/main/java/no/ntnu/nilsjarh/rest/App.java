cacf20653ae595ce77e2761ce5e94263719df2df
nilsjha <nils@gaupne.net>
2019-11-03 20:28:47 +0100

    json: tested sorting & extraction methods

--- src/main/java/no/ntnu/nilsjarh/rest/App.java
+++ src/main/java/no/ntnu/nilsjarh/rest/App.java
@@ -8,6 +8,14 @@ public class App {
         RestClient demo = new RestClient();
         demo.setEndpointUrl("datakomm.work",80,false);
         System.out.println(demo.send("dkrest/test/get"));
+        
+
+        String jsonAbc = demo.send("dkrest/test/get2");
+        JsonParser abcParse = new JsonParser();
+        System.out.println(abcParse.sortJsonString(jsonAbc));
+        System.out.println(abcParse.extractInt(jsonAbc,"a"));
+        System.out.println(abcParse.extractInt(jsonAbc,"b"));
+        System.out.println(abcParse.extractInt(jsonAbc,"c"));
     }
     
 }
